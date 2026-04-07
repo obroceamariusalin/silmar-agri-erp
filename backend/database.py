32ba6b5b019fc29f6fd1f64adfad696f3ed2587b
obroceamarius <obrocea.marius.b5s@student.ucv.ro>
2026-04-07 16:48:09 +0300

    securizare parola

--- backend/database.py
+++ backend/database.py
@@ -1,16 +1,18 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import declarative_base
-
-from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "postgresql://neondb_owner:npg_fHvdFb3WK1Tj@ep-summer-union-agkb8ihs.c-2.eu-central-1.aws.neon.tech/neondb?sslmode=require"
+load_dotenv()
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if not SQLALCHEMY_DATABASE_URL:
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
